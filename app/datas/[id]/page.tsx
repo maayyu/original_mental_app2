e7dc08c2eec9f9cf0290511e88d719a33d8f6b01
maayyu <reeeeedrabbit@yahoo.co.jp>
2024-12-22 23:07:07 +0900

    フォルダ作成

---
+++ app/datas/[id]/page.tsx
@@ -0,0 +1,14 @@
+// 日記詳細ページ
+
+"use client";
+
+import { Typography } from "@mui/material";
+
+export default function DiaryEntryPage() {
+  return (
+    <div>
+      <Typography variant="h4">Diary Entry </Typography>
+      {/* 日記の詳細を表示するためのコンテンツ */}
+    </div>
+  );
+}
---
+++ app/how-to-use/page.tsx
@@ -0,0 +1,7 @@
+// 使い方ページ
+
+import React from "react";
+
+export default function HowToUsePage() {
+  return <div>How to Use</div>;
+}
