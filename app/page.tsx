5a5f755189f686b0a978a5135ff5fb1886168c4b
maayyu <reeeeedrabbit@yahoo.co.jp>
2025-01-17 19:52:10 +0900

    pixijs 書いてみた

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 // ホーム画面
 
 import HomeMain from "./home/page";
+import PixijsForm from "../components/PixijsForm";
 
 export default function Home() {
-  return <HomeMain />;
+  return (
+    <main>
+      <PixijsForm />
+    </main>
+  );
 }
