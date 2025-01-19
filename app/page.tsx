1717cb53f7c43fc6e2fb9957adeb289dc8bf3a21
maayyu <reeeeedrabbit@yahoo.co.jp>
2025-01-19 21:41:48 +0900

    サイドバー作成

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import HomeMain from "./home/page";
 import Header from "@/components/header/Header";
+import Sidebar from "@/components/sidebar/Sidebar";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Sidebar />
       <HomeMain />
     </>
   );
