f1a6fd9b1af85f9a4d2c7bf0ccab4aa0355ba9cb
maayyu <reeeeedrabbit@yahoo.co.jp>
2025-01-19 23:47:35 +0900

    微調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,37 @@
 import Header from "@/components/header/Header";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { Box } from "@mui/material";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const isAuthPage =
+    pathname === "/auth/sign-in" || pathname === "/auth/sign-up";
+
   return (
     <html>
       <body>
-        <Box sx={{ display: "flex", height: "100vh" }}>
-          <Header />
-          <Sidebar />
-          <Box
-            component="main"
-            sx={{ flexGrow: 1, padding: "20px" }} // メインコンテンツの余白を設定
-          >
-            {children}
+        {/* ヘッダーとサイドバーを非表示にする条件 */}
+        {!isAuthPage && (
+          <Box sx={{ display: "flex", height: "100vh" }}>
+            <Header />
+            <Sidebar />
+            <Box
+              component="main"
+              sx={{
+                flexGrow: 1,
+                padding: "20px",
+                marginLeft: "250px",
+              }}
+            >
+              {children}
+            </Box>
           </Box>
-        </Box>
+        )}
+
+        {/* サインイン/サインアップページの場合 */}
+        {isAuthPage && <Box sx={{ padding: "20px" }}>{children}</Box>}
       </body>
     </html>
   );
